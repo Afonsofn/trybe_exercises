564b3d1e1e7cd1d513f246d23c4a5b9b83a16ef5
Raul <afonsoafn@gmail.com>
2020-08-31 16:12:14 +0000

    bloco 4 dia 1

---
+++ INTRODUÇÂO_AO_DESENVOLVIMENTO_WEB/bloco_4/dia_1/o_maior_numero.js
@@ -0,0 +1,7 @@
+let numero1 = 5
+let numero2 = 10
+if (numero1 > numero2) {
+    return numero1
+} else {
+    return numero2
+}
---
+++ INTRODUÇÂO_AO_DESENVOLVIMENTO_WEB/bloco_4/dia_1/triangulo.js
@@ -0,0 +1,9 @@
+let angulo1 = 60
+let angulo2 = 60
+let angulo3 = 60
+
+if (angulo1+angulo2+angulo3 == 180) {
+    return console.log("É um triangulo")
+} else {
+    return console.log("ERRO")
+}
