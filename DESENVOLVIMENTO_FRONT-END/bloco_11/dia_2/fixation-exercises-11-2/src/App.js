1439a09d21f322c9055d0adf1d08cfd15d92c17a
Afonso <afonsoafn@gmail.com>
2021-01-19 09:57:50 -0300

    Refatoraçoes

--- DESENVOLVIMENTO_FRONT-END/bloco_11/dia_2/fixation-exercises-11-2/src/App.js
+++ DESENVOLVIMENTO_FRONT-END/bloco_11/dia_2/fixation-exercises-11-2/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div>
-          <Image source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alternativeText="Cute cat staring" />  {/* Aqui estamos passando as props pra o component Image, as props sao como parametros de funções qeu podemos recuperar nos arquivos destino e usar-las */}
+          <Image source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg" alternativeText="Cute cat staring" />  {/* Aqui estamos passando as props pra o component Image, as props sao como parametros de funções que podemos recuperar nos arquivos destino e usar-las */}
         </div>
       </header>
     </div>
--- DESENVOLVIMENTO_FRONT-END/bloco_11/dia_2/fixation-exercises-11-2/src/Image.js
+++ DESENVOLVIMENTO_FRONT-END/bloco_11/dia_2/fixation-exercises-11-2/src/Image.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Image extends React.Component {
   render() {
     return <img src={this.props.source} alt={this.props.alternativeText} />; /* Props são objetos que contém todos os dados passados como parâmetro na hora de chamar um componente. */
-  }                                                                          /* Por exemplo aa props source e alternativeText foram passadas pra esse arquivo(Image) no arquivo APP */
+  }                                                                          /* Por exemplo as props source e alternativeText foram passadas pra esse arquivo(Image) no arquivo APP */
 }                                                                            /* Para recuperarmos sempre usaremos essa sintaxe "this.props.nomeDaProps" */
 
 export default Image;
