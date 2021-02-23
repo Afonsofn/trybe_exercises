d67ef7ec156d9a7944d6c8af2ea74c3e696866ed
afonso <afonsoafn@gmail.com>
2021-02-23 12:54:49 -0300

    bloco16dia3

--- DESENVOLVIMENTO_FRONT-END/bloco_16/dia_3/fixation-exercice-react-redux/src/store/index.js
+++ DESENVOLVIMENTO_FRONT-END/bloco_16/dia_3/fixation-exercice-react-redux/src/store/index.js
@@ -1,9 +1,9 @@
-import { createStore } from 'redux'
-import rootReducer from '../reducers/index';
+import { createStore } from 'redux' // Importamos o a função createStore do redux para criarmos a store.
+import rootReducer from '../reducers/index'; // Damos esse de rootReducer nome por convenção pra o arquivo que contem o combineReducers.
 
 const store = createStore(
-    rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  rootReducer, // Passamos o rootReducer pra o store
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), // essa linha habilita nos usarmos o Redux Dev Tools no chrome
 )
 
 export default store;
