ce9d5e6ed6029d571d4a135a63e0dc24993a579c
afonso <afonsoafn@gmail.com>
2021-02-23 11:30:40 -0300

    bloco16dia2pt3

--- DESENVOLVIMENTO_FRONT-END/bloco_16/dia_2/fixation-exercice-react-redux-2/src/actions/index.js
+++ DESENVOLVIMENTO_FRONT-END/bloco_16/dia_2/fixation-exercice-react-redux-2/src/actions/index.js
@@ -1,3 +1,3 @@
-const SELECT_MOVIE = 'SELECT_MOVIE';
+const SELECT_MOVIE = 'SELECT_MOVIE'; 
 
-export default SELECT_MOVIE;
+export default SELECT_MOVIE; // Criamos esse SELECT_MOVIE para usarmos nas actions só como forma de não escrever o nome errado e não percebermos.
