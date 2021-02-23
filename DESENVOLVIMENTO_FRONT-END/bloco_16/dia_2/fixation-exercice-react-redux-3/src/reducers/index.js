09e89dfc1ce8473b233660341cbd0d281278a25c
afonso <afonsoafn@gmail.com>
2021-02-23 11:11:20 -0300

    bloco16dia2pt2

--- DESENVOLVIMENTO_FRONT-END/bloco_16/dia_2/fixation-exercice-react-redux-3/src/reducers/index.js
+++ DESENVOLVIMENTO_FRONT-END/bloco_16/dia_2/fixation-exercice-react-redux-3/src/reducers/index.js
@@ -1,6 +1,8 @@
-import { combineReducers } from 'redux';
-import signalReducer from './signalReducer'
+import { combineReducers } from 'redux'; // Importamos o combineReducers do redux para unir os reducers.
+import signalReducer from './signalReducer' // Importamos os nossos reducers.
 
 export default combineReducers({
-  signal: signalReducer,  
+  signal: signalReducer,  // E passamos os reducers para o combineReducers.
 });
+
+// Usamos esse arquivo index do reducer para combinarmos todos os possíveis reducers num local só, usando o combineReducers.
